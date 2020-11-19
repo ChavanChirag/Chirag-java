70e110822148e195af9a9f0ff98cf61c6b010e0b
ChiragChavan <73816709+ChavanChirag@users.noreply.github.com>
2020-11-19 20:12:16 +0530

    Update Practical 1.java

--- Practical 1.java
+++ Practical 1.java
@@ -1,18 +1,8 @@
-import java.util.Scanner;
-public class Addition {
-    public static void main(String args[]) {
-        Scanner scan= new Scanner(System.in);
-        try{
-            System.out.println("Enter a number:");
-            int a=scan.nextInt();
-            System.out.println("Enter another number:");
-            int b=scan.nextInt();
-            System.out.println(a+b);
-        }
-        catch(Exception e)
-        {
-            System.out.println("Error: "+e);
-        }
-    }
-    
-}
+public class My class{
+       public static void main(String args[]}
+ {    int x=10;
+       int y=15;
+       int z=x+y;
+
+       System.out.println(" Sum of x+y= " +z);
+  }
